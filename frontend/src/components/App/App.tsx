a0883fa62fdd6989e0099cc0c504c8beb6e0698c
Marcos <e.marcosra@gmail.com>
2025-03-12 07:42:41 +0100

    Add toasts to create and delete snippets

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SnippetList from '../SnippetList/SnippetList';
+import { ToastContainer } from 'react-toastify';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<SnippetList />} />
       </Routes>
